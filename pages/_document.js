e5b5e9f77981a2af0b5bf96bc779557b06ed746c
tamo2web3 <114233287+tamo2web3@users.noreply.github.com>
2022-12-18 18:30:54 +0900

    update semantic-css
    update semantic-css

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+    <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
       <Head />
       <body>
         <Main />
