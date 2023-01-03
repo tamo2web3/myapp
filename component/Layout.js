54fdd45c93035b167a51e149e2d2490298e4c602
tamo2web3 <114233287+tamo2web3@users.noreply.github.com>
2023-01-03 19:41:22 +0900

    heal mj try comment layout

--- component/Layout.js
+++ component/Layout.js
@@ -8,6 +8,7 @@ const Layout = (props) => {
     <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
     <Header />
       {props.children}
+      ※　スマホは、横向きでお願いします。
       <List>
         <List.Item>
           <List.Icon name='mail' />
--- pages/heal/indexh_mj.js
+++ pages/heal/indexh_mj.js
@@ -48,7 +48,7 @@ class testIndex extends Component {
           </Grid.Column>
           <Grid.Column>
 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12970.718126694015!2d139.6392271078125!3d35.6356261!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f48420960a05%3A0x8f13b2c06f2abc81!2z5ZCR5LqV5r2k5ZCJ44Ki44OI44Oq44Ko6aSo!5e0!3m2!1sja!2sjp!4v1672169116225!5m2!1sja!2sjp" width="450" height="338"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12970.718126694015!2d139.6392271078125!3d35.6356261!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f48420960a05%3A0x8f13b2c06f2abc81!2z5ZCR5LqV5r2k5ZCJ44Ki44OI44Oq44Ko6aSo!5e0!3m2!1sja!2sjp!4v1672169116225!5m2!1sja!2sjp" width="75%" height="100%"></iframe>
 
           </Grid.Column>
 
